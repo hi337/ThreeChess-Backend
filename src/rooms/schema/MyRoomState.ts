25c240038e20de675bdea6772ad27547ddb209f5
Rateb Ajamieh <ajamiehrateb@gmail.com>
2023-05-16 14:37:58 -0600

    started making schema

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,37 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { Schema, Context, type, ArraySchema } from "@colyseus/schema";
+
+export class Position extends Schema {
+  @type("number")
+  x: number;
+
+  @type("number")
+  y: number;
+
+  constructor(x: number = 0, y: number = 0) {
+    super();
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class Piece extends Schema {
+  @type(Position)
+  pos: Position;
+
+  @type("string")
+  team: string;
+
+  constructor(pos: Position) {
+    super();
+    this.pos = new Position();
+  }
+}
 
 export class MyRoomState extends Schema {
+  @type([Piece])
+  pieces_array: Piece[];
 
-  @type("string") mySynchronizedProperty: string = "Hello world";
-
+  constructor() {
+    super();
+  }
 }
